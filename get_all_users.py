0475da15048bce9a8e3c8bd27a6684fe98cc8b74
Sorin <dsorin95@gmail.com>
2021-12-14 22:30:30 +0100

    Replaced hardcoded string with constant. The API Token is now read from the token file.

--- get_all_users.py
+++ get_all_users.py
@@ -5,16 +5,30 @@ and write all the data to a json file for future usage.
 
 import requests
 import json
+import os.path
+import constants
 
-#Use your personal token to request all the profiles from the network.
-token = "insert token obtained from login.py"
-url = "http://35.242.203.63/api/pre/DEVGetAllProfiles.php"
-request = requests.post(url, headers = {"Authorization": token})
+#Paste your token here, or let the script read it from the token file.
+token = None
+
+#Try to read the token from file.
+if os.path.isfile(constants.TOKEN_FILE):
+    with open(constants.TOKEN_FILE) as file:
+        token = file.readline()
+
+if not token:
+    print("Use login.py to log into the network and get your API Token first.")
+    exit()
+
+#Make a POST request to the server to get the users.
+request = requests.post(constants.GET_ALL_USERS_URL, headers = {"Authorization": token})
 response = request.json()
 usersArray = response["message"] #The message field will contain an array with all the users in the network.
 
-#Write all users data to a json file.
-file = open("firespark_users.json", "w")
+#Write all the users data to a json file.
+file = open(constants.USERS_FILE, "w")
 
 file.write(json.dumps(usersArray, indent = 4))
 file.close()
+
+print("Users data has been written to", constants.USERS_FILE)
