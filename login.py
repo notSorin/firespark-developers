4ff7fd6819022e44974497524b78e861afd2cfb0
Sorin <dsorin95@gmail.com>
2021-12-14 22:28:51 +0100

    Replaced hardcoded string with constant. The token is now automatically written to a file to hold it for later usage.

--- login.py
+++ login.py
@@ -4,16 +4,21 @@ and how to grab the token required for future requests.
 """
 
 import requests
+import constants
 
 #Make a POST request to log in using your email (or username) and password.
-url = "http://35.242.203.63/api/pre/LoginUser.php"
 postData = {
     "email_or_username": "insert email or username",
     "password": "insert password"
     }
 
-response = requests.post(url, data = postData)
+response = requests.post(constants.LOG_IN_URL, data = postData).json()
 
 #The response will contain a "token" key; make a copy of it,
 #and keep it secure; it will be needed for future requests.
-print(response.json())
+print(response)
+
+#Write token to file.
+with open(constants.TOKEN_FILE, "w") as file:
+    file.write(response["message"]["token"])
+    print("API Token has been written to", constants.TOKEN_FILE)
